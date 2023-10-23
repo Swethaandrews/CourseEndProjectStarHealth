5dc655cc24a45997b9b1ab3f9470bd46fa3aeb65
swethaandrews <andrews@gmail.com>
2023-10-23 23:02:06 +0530

    done

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"a6412ffa-534c-4055-8d94-46cc2ecb6ebc","feature":"Test the starHealth page on Chrome Browser","scenario":"Validate the Star Health Buy Now flow","start":1698081707096,"group":1,"content":"","tags":"","end":1698081746463,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
